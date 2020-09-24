389c4bf35f9dcd8fe7521a15253f12e7576ae00a
Swaroopa tirumalareddy <swaroopatirumalareddy@gmail.com>
2020-09-24 11:07:19 -0500

    Coded the hello world button

--- HelloWorld and Emojji/HelloWorld and Emojji/ViewController.swift
+++ HelloWorld and Emojji/HelloWorld and Emojji/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
